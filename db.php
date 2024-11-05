192dd408b2c62da8272bb9efbc69fc2f0e3dc061
remwina <jaikajeon@gmail.com>
2024-11-05 21:34:25 +0800

    added db file

---
+++ db.php
@@ -0,0 +1,45 @@
+<?php
+
+$db_server = "localhost";
+    $db_user = "admin";
+    $db_pass = "password";
+    $db_name = "user_login";
+    $connection = " ";
+
+    try{
+        $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name, 3306);
+    }
+    catch (mysqli_sql_exception){
+        echo ' ';
+    }
+
+    // if($connection){
+    //     echo "Connection Successful";
+    // }
+    // else{
+    //     echo "Failed to connect";
+    // }
+
+?><?php
+
+$db_server = "localhost";
+    $db_user = "admin";
+    $db_pass = "password";
+    $db_name = "user_login";
+    $connection = " ";
+
+    try{
+        $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name, 3306);
+    }
+    catch (mysqli_sql_exception){
+        echo ' ';
+    }
+
+    // if($connection){
+    //     echo "Connection Successful";
+    // }
+    // else{
+    //     echo "Failed to connect";
+    // }
+
+?>
