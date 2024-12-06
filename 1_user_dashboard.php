b2d26309e17d142dea934471186c67f70d531b1e
JohnLorcanParaiso <johnlorcparadise@gmail.com>
2024-12-06 21:27:41 +0800

    updated folders

--- 1_user_dashboard.php
+++
@@ -1,8 +0,0 @@
-// ... existing code ...
-<td>
-    <?= htmlspecialchars($row['cat_name']) ?>
-    <?php if (!empty($row['edited_at'])): ?>
-        <span class="badge text-white" style="background-color: #6f42c1; font-size: 0.65rem;">Edited</span>
-    <?php endif; ?>
-</td>
-// ... existing code ...
