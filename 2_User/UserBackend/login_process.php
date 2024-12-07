961cd2c7e00603bacadc73546d8ecdf2203a3def
Jaika Remina Madrid <159684743+remwina@users.noreply.github.com>
2024-12-07 23:02:30 +0800

    Delete 2_User directory

--- 2_User/UserBackend/login_process.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// In your login process, after successful login
-$_SESSION['profile_image'] = $user['profile_image'];
-
-?> 
--- 2_User/UserBackend/update_profile.php
+++
@@ -1,4 +0,0 @@
-<?php
-// After successful profile image update
-$_SESSION['profile_image'] = $newImageName; // Update session with new image
-?> 
